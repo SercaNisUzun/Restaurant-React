d298103b352b3a09f770bc0bde73794db255e699
SercaNisUzun <124430337+SercaNisUzun@users.noreply.github.com>
2025-03-05 02:01:41 +0300

    Router

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
 
-    <BrowserRouter>
+    <BrowserRouter basename='/Restaurant-React/'>
       <App />
     </BrowserRouter>
 
